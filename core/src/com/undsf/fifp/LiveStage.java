677d74495c50c0f7ce7df874b974016c9e0ef134
Arathi <ssdfa7@gmail.com>
2016-01-06 10:49:16 +0800

    添加了触击音效以及中心音符的心跳
    Signed-off-by: Arathi <ssdfa7@gmail.com>

--- core/src/com/undsf/fifp/LiveStage.java
+++ core/src/com/undsf/fifp/LiveStage.java
@@ -1,5 +1,9 @@
 package com.undsf.fifp;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.audio.Sound;
+import com.badlogic.gdx.files.FileHandle;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.scenes.scene2d.Action;
@@ -21,6 +25,12 @@ public class LiveStage extends Stage {
     private List<Tap> taps;
     private double tempo; //速率
     private double perfectTime;
+    private Music music;
+    private Sound sePerfect;
+    private Sound seGreat;
+    private Sound seGood;
+    private Sound seBad;
+    private Sound seMiss;
 
     public LiveStage() {
         Texture backgroundTexture = new Texture("backgrounds/06.png");
@@ -30,14 +40,26 @@ public class LiveStage extends Stage {
         scorebar = new Image(scorebarTexture);
         tapStartPoint = new Image(tapStartTexture);
 
+        FileHandle fh = Gdx.files.internal("se/perfect.ogg");
+        sePerfect = Gdx.audio.newSound(fh);
+
         perfectTime = 1.0; //Hard级，暂定1秒
         double fullActionTime = perfectTime * Constants.FULL_ACTION_RATE;
         double fullActionLength = Constants.PERFECT_LENGTH * Constants.FULL_ACTION_RATE;
 
         background.setPosition(0, 0);
+
         tapStartPoint.setOrigin(tapStartTexture.getWidth()/2, tapStartTexture.getHeight()/2);
         tapStartPoint.setPosition(480-tapStartTexture.getWidth()/2, 480-tapStartTexture.getHeight()/2);
         tapStartPoint.setScale(0.5f);
+        Action heartbeatOnce = Actions.sequence(
+                Actions.scaleTo(0.75f, 0.75f, 0.15f),
+                Actions.scaleTo(0.5f,0.5f,0.55f),
+                Actions.delay(0.25f)
+        );
+        Action heartbeats = Actions.forever(heartbeatOnce);
+        tapStartPoint.addAction(heartbeats);
+
         scorebar.setPosition(0, Constants.CANVAS_HEIGHT - scorebar.getHeight());
         this.addActor(background);
         this.addActor(scorebar);
@@ -76,6 +98,7 @@ public class LiveStage extends Stage {
     public boolean touchDown(int screenX, int screenY, int pointer, int button) {
         super.touchDown(screenX, screenY, pointer, button);
         System.out.println("touchDown: " + screenX + "," + screenY);
+        sePerfect.play();
         return false;
     }
 
